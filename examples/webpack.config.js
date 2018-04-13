574fb81ee575c5173e4f10823a5abbd6cd5db918
Héctor Ramón Jiménez <hector0193@gmail.com>
2018-04-13 23:51:10 +0200

    Add live demo code

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../docs'),
     publicPath: '/elm-wip-form/',
     filename: '[name].[hash].js'
   },
